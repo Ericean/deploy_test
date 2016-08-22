133086cbb73c0b7f3140100d386c47c054cd750c
Ze <ze_gao@163.com>
2016-08-22 11:59:04 +0800

    all set

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+# config valid only for current version of Capistrano
+lock '3.6.0'
+
+set :application, 'deploy_test'
+set :repo_url, 'git@github.com:Ericean/deploy_test.git'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/deploy/test'
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
